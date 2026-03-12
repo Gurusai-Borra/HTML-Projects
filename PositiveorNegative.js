b385e933346caab01ac181a61410cf165474ca17
Gurusai-Borra <saibguru2@gmail.com>
2026-03-12 10:42:55 +0530

    Positive or negative

---
+++ PositiveorNegative.js
@@ -0,0 +1,10 @@
+console.log("Checking a Number Positive Or Negative")
+let n=100
+if(n<0){
+    console.log("Your Number is Negative:",n)
+}
+else if(n>0){
+    console.log("Your Number is Positive:",n)
+}else{
+    console.log("Your number is Zero",n)
+}
---
+++ evenorodd.js
@@ -0,0 +1,8 @@
+console.log("Checking the input is Even or Odd",n)
+let n=3
+if (n % 2==0){
+    console.log("It is a Even Number",n)
+}
+else{
+    console.log("It is a odd Number",n)
+}
